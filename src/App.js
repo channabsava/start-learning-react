00da02f83bdbe3547adccf9f1491788d6e0b005b
channabsava <channu.nhongunti@gmail.com>
2019-11-07 16:55:19 +0530

    created first react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+//import logo from "./logo.svg";
+import bg from "./img/bg.jpg";
+import "./App.css";
+import Greet from "./component/Greet";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="card">
+      <div class="container">
+        <div class="infocontainer">
+          <div class="number">
+            12<sup>o</sup>
+          </div>
+          <div>
+            <div class="text1">Scattered Cloud</div>
+            <div class="text">Mississauga/Canada</div>
+          </div>
+          <div class="time">
+            9:38 <span class="spanpropP">p.m.</span>
+          </div>
+
+          <div class="day">Monday,03</div>
+        </div>
+      </div>
+
+      <div class="icon">
+        <div class="square1"></div>
+        <div class="square2"></div>
+        <div class="square3"></div>
+        <div class="square4"></div>
+        <Greet></Greet>
+      </div>
     </div>
   );
 }
