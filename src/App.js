984d4fb6459910c193ccb0bc242bc26977e6c474
channabsava <channu.nhongunti@gmail.com>
2019-11-12 17:02:35 +0530

    lifeCycle added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "react"
+    };
+  }
+
+  componentDidMount() {
+    console.log("Component did mount");
+  }
+  componentWillMount() {
+    console.log("component will mount");
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount()");
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate()");
+    return this.state.name !== nextState.name;
+  }
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <p>Start editing to see some magic happen :)</p>
+          <button onClick={() => this.setState({ name: "anil" })}>click</button>
+          <button onClick={() => this.setState({ name: "react" })}>
+            click me
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Lifecycle.js
+++ src/Lifecycle.js
@@ -1,8 +1,48 @@
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0
-    };
-  }
-}
+// import React, { Component } from "react";
+
+// import Hello from "./Hello";
+
+// export default class Lifecycle extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       name: "react"
+//     };
+//   }
+//   componentDidMount() {
+//     console.log("Component did mount");
+//   }
+//   componentWillMount() {
+//     console.log("component will mount");
+//   }
+//   componentDidUpdate() {
+//     console.log("componentDidUpdate");
+//   }
+//   componentWillUnmount() {
+//     console.log("componentWillUnmount()");
+//   }
+//   shouldComponentUpdate(nextProps, nextState) {
+//     console.log("shouldComponentUpdate()");
+//     return this.state.name !== nextState.name;
+//   }
+//   getDerivedStateFromProps(props, state) {
+//     console.log("getDerivedStateFromProps(props, state) ");
+//     return null;
+//   }
+
+//   render() {
+//     console.log("render");
+//     return (
+//       <div>
+//         <Hello name={this.state.name} />
+//         <p>Start editing to see some magic happen :)</p>
+//         <button onClick={() => this.setState({ name: "anil" })}>
+//           click me
+//         </button>
+//         <button onClick={() => this.setState({ name: "react" })}>
+//           click me
+//         </button>
+//       </div>
+//     );
+//   }
+// }
