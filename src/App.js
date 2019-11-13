41206c608ed20d904fcb678df5f148735011667c
channabsava <channu.nhongunti@gmail.com>
2019-11-13 11:54:06 +0530

    fetchAPI and diplsyed imges

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
+import style from "./App.module.css";
 
-function App() {
-  return <div className="App"></div>;
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = { data: "" };
+  }
+  componentDidMount() {
+    console.log("Component did mount ");
+    fetch("https://api.github.com/users")
+      .then(result => result.json())
+      .then(data => {
+        this.setState({ data });
+        console.log(this.state.data);
+      });
+  }
+  render() {
+    return (
+      <div className={style.base}>
+        {this.state &&
+          this.state.data &&
+          this.state.data.map(data => {
+            return (
+              <div>
+                <div>
+                  <img src={data.avatar_url} alt="profile"></img>
+                </div>
+                <div className={style.name}>{data.login}</div>
+              </div>
+            );
+          })}
+      </div>
+    );
+  }
 }
-
-export default App;
