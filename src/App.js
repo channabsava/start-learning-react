cd7d44d61ea5441fbe24345355e6a7ce489213aa
channabsava <channu.nhongunti@gmail.com>
2019-11-19 15:58:23 +0530

    React-Router-App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from "react";
 import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
 
+import Home from "./Component/Home.js";
+import About from "./Component/About.js";
+import Contact from "./Component/Contact.js";
+import NotFound from "./Component/NotFound";
+
+import React from "react";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Contact" component={Contact} />
+        <Route path="/NotFound" component={NotFound} />
+        <Route>
+          <Redirect to="/NotFound"></Redirect>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
