093f553405f91708d001fed25411a0f52502c79e
channabsava <channu.nhongunti@gmail.com>
2019-11-19 14:30:55 +0530

    changes are done

--- src/Action/Todo.Action.js
+++ src/Action/Todo.Action.js
@@ -1,6 +1,3 @@
-export const SET_VISIBILITYFILTER = "SET_VISIBILITYFILTER";
-export const TOGGLE_TODO = "TOGGLE_TODO";
-export const VISIBILITYFILTER = "VISIBILITYFILTER";
 export const ADD_TODO = "ADD_TODO";
 export const DELETE_TODO = "DELETE_TODO";
 let nextTodoId = 0;
@@ -14,7 +11,6 @@ export function addTodo(text) {
 }
 
 export function deleteTodo(id) {
-  console.log(id, "action");
   return {
     type: DELETE_TODO,
     id
