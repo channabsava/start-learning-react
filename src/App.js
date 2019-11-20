1227bf6250af5c43dd1840e4cb998b6d25630311
channabsava <channu.nhongunti@gmail.com>
2019-11-20 14:33:20 +0530

    TodoList

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
+import CreateTodo from "./Container /CreateTodo";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <CreateTodo />
+      </div>
+    );
+  }
 }
-
 export default App;
